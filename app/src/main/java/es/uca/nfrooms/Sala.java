66f380f9d99aa546a8fde2adb80fe6deaa7033b4
Argudo <josexerezano7@gmail.com>
2022-05-17 00:39:26 +0200

    Salas v0.1.2

--- app/src/main/java/es/uca/nfrooms/Sala.java
+++ app/src/main/java/es/uca/nfrooms/Sala.java
@@ -22,8 +22,37 @@ public class Sala extends AppCompatActivity {
 
         switch (sala){
             case "Kyoto":
-                SpannableString desc = new SpannableString("Descripción: " + getResources().getString(R.string.desc_kyoto));
+                SpannableString nom = new SpannableString("Nombre: Sala Kyoto");
+                nom.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), 0, 7, 0);
+                nombre.setText(nom);
 
+                SpannableString desc = new SpannableString("Descripción: " + getResources().getString(R.string.desc_kyoto));
+                desc.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), 0, 11, 0);
+                descripcion.setText(desc);
+
+                SpannableString cap = new SpannableString("Capacidad: 50 personas");
+                cap.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), 0, 9, 0);
+                capacidad.setText(cap);
+
+                SpannableString price = new SpannableString("Precio: 1050€");
+                price.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), 0, 7, 0);
+                precio.setText(price);
+            case "Miami":
+                nom = new SpannableString("Nombre: Sala Miami");
+                nom.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), 0, 7, 0);
+                nombre.setText(nom);
+
+                desc = new SpannableString("Descripción: " + getResources().getString(R.string.desc_miami));
+                desc.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), 0, 11, 0);
+                descripcion.setText(desc);
+
+                cap = new SpannableString("Capacidad: 2 personas");
+                cap.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), 0, 9, 0);
+                capacidad.setText(cap);
+
+                price = new SpannableString("Precio: 40€");
+                price.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), 0, 7, 0);
+                precio.setText(price);
         }
     }
 }
