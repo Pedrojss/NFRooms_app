f62a427e966d01aa4e584c936521446e4b2bc7c7
Argudo <josexerezano7@gmail.com>
2022-05-17 00:01:37 +0200

    Salas v0.1

--- app/src/main/java/es/uca/nfrooms/MainActivity.java
+++ app/src/main/java/es/uca/nfrooms/MainActivity.java
@@ -3,6 +3,7 @@ package es.uca.nfrooms;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -47,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
     }
 
     @Override
--- app/src/main/java/es/uca/nfrooms/ui/salas/SalasFragment.java
+++ app/src/main/java/es/uca/nfrooms/ui/salas/SalasFragment.java
@@ -1,15 +1,20 @@
 package es.uca.nfrooms.ui.salas;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import es.uca.nfrooms.R;
+import es.uca.nfrooms.Sala;
 import es.uca.nfrooms.databinding.FragmentSalasBinding;
 
 public class SalasFragment extends Fragment {
@@ -23,6 +28,17 @@ public class SalasFragment extends Fragment {
 
         binding = FragmentSalasBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+
+        TextView linkKyoto = (TextView) root.findViewById(R.id.linkKyoto);
+        linkKyoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sala = new Intent(getActivity(), Sala.class);
+                sala.putExtra("sala", "Kyoto");
+                startActivity(sala);
+            }
+        });
 
         return root;
     }
