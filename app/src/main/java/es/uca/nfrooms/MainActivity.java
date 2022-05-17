ace0f895204a213bf3a88dc665597108b96e667f
argudo <jose.argudogarrido@alum.uca.es>
2022-05-17 22:12:18 +0200

    Atajo home

--- app/src/main/java/es/uca/nfrooms/MainActivity.java
+++ app/src/main/java/es/uca/nfrooms/MainActivity.java
@@ -1,5 +1,6 @@
 package es.uca.nfrooms;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -33,8 +34,9 @@ public class MainActivity extends AppCompatActivity {
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent home = new Intent(view.getContext(), MainActivity.class);
+                startActivity(home);
+                finish();
             }
         });
         DrawerLayout drawer = binding.drawerLayout;
