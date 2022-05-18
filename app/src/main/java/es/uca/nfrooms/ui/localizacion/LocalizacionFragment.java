a148dadda868f3db5128c2448fcdd69633442d1b
jagc55 <juanantonio.gonzalezcantero@alum.uca.es>
2022-05-18 03:07:32 +0200

    localizacion avances

--- app/src/main/java/es/uca/nfrooms/ui/localizacion/LocalizacionFragment.java
+++ app/src/main/java/es/uca/nfrooms/ui/localizacion/LocalizacionFragment.java
@@ -7,31 +7,44 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import es.uca.nfrooms.databinding.FragmentLocalizacionBinding;
 
-public class LocalizacionFragment extends Fragment {
+public class LocalizacionFragment extends SupportMapFragment implements OnMapReadyCallback {
 
-    private FragmentLocalizacionBinding binding;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        LocalizacionViewModel localizacionViewModel =
-                new ViewModelProvider(this).get(LocalizacionViewModel.class);
+        getMapAsync(this);
 
-        binding = FragmentLocalizacionBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textLocalizacion;
-        localizacionViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return rootView;
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onMapReady(GoogleMap map) {
+        // Posicionar el mapa en una localización y con un nivel de zoom
+        LatLng latLng = new LatLng(36.679582, -5.444791);
+        // Un zoom mayor que 13 hace que el emulador falle, pero un valor deseado para
+        // callejero es 17 aprox.
+        float zoom = 13;
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+
+        // Colocar un marcador en la misma posición
+        map.addMarker(new MarkerOptions().position(latLng));
+        // Más opciones para el marcador en:
+        // https://developers.google.com/maps/documentation/android/marker
+
+        // Otras configuraciones pueden realizarse a través de UiSettings
+        // UiSettings settings = getMap().getUiSettings();
     }
+
 }
