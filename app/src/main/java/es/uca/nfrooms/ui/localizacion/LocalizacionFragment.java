906d4bc2386626130a51e6d675e3cde8d4683edb
jagc55 <juanantonio.gonzalezcantero@alum.uca.es>
2022-05-18 03:59:42 +0200

    En teoria el mapa esta hecho pero no se ve

--- app/src/main/java/es/uca/nfrooms/ui/localizacion/LocalizacionFragment.java
+++ app/src/main/java/es/uca/nfrooms/ui/localizacion/LocalizacionFragment.java
@@ -1,50 +1,38 @@
 package es.uca.nfrooms.ui.localizacion;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
-import androidx.annotation.NonNull;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
+import es.uca.nfrooms.MapsActivity;
+import es.uca.nfrooms.R;
+import es.uca.nfrooms.crearReserva;
 import es.uca.nfrooms.databinding.FragmentLocalizacionBinding;
 
-public class LocalizacionFragment extends SupportMapFragment implements OnMapReadyCallback {
+
+public class LocalizacionFragment extends Fragment {
+    private FragmentLocalizacionBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        binding = FragmentLocalizacionBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-        getMapAsync(this);
+        Button mButton = (Button)root.findViewById(R.id.mButton);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        return rootView;
-    }
-
-    @Override
-    public void onMapReady(GoogleMap map) {
-        // Posicionar el mapa en una localización y con un nivel de zoom
-        LatLng latLng = new LatLng(36.679582, -5.444791);
-        // Un zoom mayor que 13 hace que el emulador falle, pero un valor deseado para
-        // callejero es 17 aprox.
-        float zoom = 13;
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
-
-        // Colocar un marcador en la misma posición
-        map.addMarker(new MarkerOptions().position(latLng));
-        // Más opciones para el marcador en:
-        // https://developers.google.com/maps/documentation/android/marker
-
-        // Otras configuraciones pueden realizarse a través de UiSettings
-        // UiSettings settings = getMap().getUiSettings();
+        return root;
     }
 
 }
